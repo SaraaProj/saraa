1b4af54c693ff38e0189e302ba1b9ad57d2b9f5b
shoosaka <foa.sho0111@outlook.jp>
2025-04-26 07:19:59 +0900

    fix: testcase

--- __tests__/api/hello.test.ts
+++ __tests__/api/hello.test.ts
@@ -1,31 +1,21 @@
-import { expect, test } from 'vitest'
-import * as appHandler from '../../app/api/hello/route'
-import { testApiHandler } from 'next-test-api-route-handler'
+import { expect, test } from "vitest";
+import * as appHandler from "../../app/api/hello/route";
+import { testApiHandler } from "next-test-api-route-handler";
 
 /**
  * NOTE: サンプルのAPIルートのテスト
- * SampleAPIで messageが"Helli, world!"であるか
+ * SampleAPIで messageが"Hello, world!"であるか
  */
-test('GET /api/hello', async () => {
+test("GET /api/hello", async () => {
   await testApiHandler({
     appHandler,
-    // 任意のリクエストヘッダを追加する
-    requestPatcher(request) {
-      // TODO: 認証やその他のヘッダを追加する場合に利用する
-      request.headers.set('key', process.env.SPECIAL_TOKEN!)
-    },
-    async responsePatcher(response) {
-      const json = await response.json()
-      return Response.json(
-        json.apiSuccess ? { ...json, apiSuccess: true } : json,
-      )
-    },
-    async test({ fetch }) {
-      const res = await fetch({ method: 'GET' })
-      await expect(res.json()).resolves.toStrictEqual({
-        message: 'Hello, world!',
+    test: async ({ fetch }) => {
+      const res = await fetch({ method: "GET" });
+      const json = await res.json();
+      expect(json).toEqual({
+        message: "Hello, world!",
         query: {},
-      })
+      });
     },
-  })
-})
+  });
+});
